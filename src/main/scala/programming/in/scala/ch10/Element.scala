140e370c2080fe044bda7e8f7b62aa976ce4f556
james.dunnam <jamesd1184@gmail.com>
2016-05-25 09:03:21 -0400

    finishing up ch 10

--- src/main/scala/programming/in/scala/ch10/Element.scala
+++ src/main/scala/programming/in/scala/ch10/Element.scala
@@ -1,5 +1,6 @@
 package programming.in.scala.ch10
 
+import Element.elem
 /**
   * Created by james on 5/24/2016.
   */
@@ -14,21 +15,61 @@ abstract class Element {
     println("Element's implementation invoked")
   }
 
-  def above(that: Element): Element =
-    new ArrayElement(this.contents ++ that.contents)
+  def above(that: Element): Element = {
+    val this1 = this widen that.width
+    val that1 = that widen this.width
+    elem(this1.contents ++ that1.contents)
+  }
 
-  def beside(that: Element): Element =
-    new ArrayElement(
-      for (
-        (line1, line2) <- this.contents zip that.contents
-      ) yield line1 + line2
-    )
+  def beside(that: Element): Element = {
+    val this1 = this heighten that.height
+    val that1 = that heighten this.height
+    elem(
+      for ((line1, line2) <- this1.contents zip that1.contents)
+        yield line1 + line2)
+  }
+
+  def widen(w: Int): Element =
+    if (w <= width) this
+    else {
+      val left = elem(' ', (w - width) / 2, height)
+      var right = elem(' ', w - width - left.width, height)
+      left beside this beside right
+    }
+
+  def heighten(h: Int): Element =
+    if (h <= height) this
+    else {
+      val top = elem(' ', width, (h - height) / 2)
+      var bot = elem(' ', width, h - height - top.height)
+      top above this above bot
+    }
 
   override def toString = contents mkString "\n"
 }
 
 object Element {
-  def elem(contents: Array[String]): Element =
+
+  private class ArrayElement(
+                              val contents: Array[String]
+                            ) extends Element
+
+  private class LineElement(s: String) extends Element {
+    val contents = Array(s)
+    override def width = s.length
+    override def height = 1
+  }
+
+  private class UniformElement(
+                                ch: Char,
+                                override val width: Int,
+                                override val height: Int
+                              ) extends Element {
+    private val line = ch.toString * width
+    def contents = Array(height.toString, line)
+  }
+
+  def elem(contents:  Array[String]): Element =
     new ArrayElement(contents)
 
   def elem(chr: Char, width: Int, height: Int): Element =
